91288e8805b63816cec6532adf69f158777f30ad
Adrian Villa <adriancvilla@gmail.com>
2023-02-09 18:06:29 -0500

    feat: mockup login page and add react cookie logic for nickname

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,40 @@
-const Login = () => {
+import styles from './styles.module.css';
+import { useCookies } from 'react-cookie';
+import { useState } from 'react';
 
-<>
-  <div>
-      <h2></h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae deleniti, ea iste dicta sint similique accusamus autem numquam libero, labore mollitia magni eum, repellat iusto distinctio temporibus animi pariatur quaerat.</p>
-      <input type="text" placeholder="Insert your nickname" />
-      <button>Join</button>
-  </div>
-</>
-}
+const Login = () => {
+  const [nickname, setNickname] = useState('');
+  const [cookie, setCookie] = useCookies();
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setNickname(e.target.value);
+  };
+
+  const handleNameToCookies = (): void => {
+    setCookie('nickname', nickname, { path: '/' });
+  };
+
+  return (
+    <>
+      <div className={styles.login_container}>
+        <h2 className={styles.container_title}>Welcome to real time chat</h2>
+        <p className={styles.container_description}>
+          A random chat to chat random people.
+        </p>
+        <form className={styles.container_form}>
+          <input
+            className={styles.form_nickname}
+            onChange={handleInputChange}
+            type="text"
+            placeholder="Insert your nickname"
+          />
+          <button className={styles.form_submit} onClick={handleNameToCookies}>
+            Join
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Login;
