f7fe3461e51c167a8b89ce557e5ff6fa936c0f55
Adrian Villa <adriancvilla@gmail.com>
2023-02-09 20:54:18 -0500

    feat: add style base to login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -16,22 +16,27 @@ const Login = () => {
 
   return (
     <>
-      <div className={styles.login_container}>
-        <h2 className={styles.container_title}>Welcome to real time chat</h2>
-        <p className={styles.container_description}>
-          A random chat to chat random people.
-        </p>
-        <form className={styles.container_form}>
-          <input
-            className={styles.form_nickname}
-            onChange={handleInputChange}
-            type="text"
-            placeholder="Insert your nickname"
-          />
-          <button className={styles.form_submit} onClick={handleNameToCookies}>
-            Join
-          </button>
-        </form>
+      <div className={styles.parent_login_container}>
+        <div className={styles.login_container}>
+          <h2 className={styles.container_title}>Welcome to Real time chat</h2>
+          <p className={styles.container_description}>
+            A random chat to chat for random people.
+          </p>
+          <form className={styles.container_form}>
+            <input
+              className={styles.form_nickname}
+              onChange={handleInputChange}
+              type="text"
+              placeholder="Insert your nickname"
+            />
+            <button
+              className={styles.form_submit}
+              onClick={handleNameToCookies}
+            >
+              Join
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
